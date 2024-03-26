315ef1b72de1676cf3a9c7e65d4fa66c6009ea0b
Franco Silva <franco_tigres18@hotmail.com>
2024-03-26 08:49:28 -0600

    feat(Agregar y Decrementar item carrito)

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
 
+  const MAX_ITEMS = 5
+
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
     if (itemExists >= 0) {/* Existe en el carrito */
@@ -26,11 +28,39 @@ function App() {
     setCart((prevCart) => prevCart.filter(guitar => guitar.id !== id))
   }
 
+  function increaseQuantity(id) {
+    const updateCart = cart.map(item => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function decrementQuantity(id) {
+    const updateCart = cart.map(item => {
+      if (item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
   return (
     <>
       <Header
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
       />
 
       <main className="container-xl mt-5">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 import { useMemo } from "react";
 
-const Header = ({ cart, removeFromCart }) => {
+const Header = ({ cart, removeFromCart, increaseQuantity, decrementQuantity }) => {
 
   // State Derivado
   const isEmpty = useMemo(() => cart.length === 0, [cart])
@@ -55,6 +55,7 @@ const Header = ({ cart, removeFromCart }) => {
                               <button
                                 type="button"
                                 className="btn btn-dark"
+                                onClick={() => decrementQuantity(guitar.id)}
                               >
                                 -
                               </button>
@@ -62,6 +63,7 @@ const Header = ({ cart, removeFromCart }) => {
                               <button
                                 type="button"
                                 className="btn btn-dark"
+                                onClick={() => increaseQuantity(guitar.id)}
                               >
                                 +
                               </button>
@@ -70,7 +72,7 @@ const Header = ({ cart, removeFromCart }) => {
                               <button
                                 className="btn btn-danger"
                                 type="button"
-                                onClick={()=> removeFromCart(guitar.id)}
+                                onClick={() => removeFromCart(guitar.id)}
                               >
                                 X
                               </button>
